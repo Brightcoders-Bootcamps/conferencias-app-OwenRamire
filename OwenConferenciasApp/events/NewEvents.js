8934e335be4f67e2a7a5bcfbdca4435e1d5351e4
OwenRamire <owenomar_ramirez@ucol.mx>
2020-07-16 18:26:42 -0500

    Create new and old events to test

---
+++ OwenConferenciasApp/events/NewEvents.js
@@ -0,0 +1,26 @@
+export const NewEvents = [
+  {
+    key: '0',
+    date: '2020-07-06',
+    nameEvent: 'The Spoils of war ',
+    hour: '09:00 - 10:00',
+  },
+  {
+    key: '1',
+    date: '2020-07-07',
+    nameEvent: 'The broken man',
+    hour: '09:00 - 10:00',
+  },
+  {
+    key: '2',
+    date: '2020-07-08',
+    nameEvent: 'Back to winterfell',
+    hour: '09:00 - 10:00',
+  },
+  {
+    key: '3',
+    date: '2020-07-08',
+    nameEvent: 'Hello',
+    hour: '09:00 - 10:00',
+  },
+];
---
+++ OwenConferenciasApp/events/OldEvents.js
@@ -0,0 +1,8 @@
+export const OldEvents = [
+  {
+    key: '0',
+    date: '2020-04-10',
+    nameEvent: 'The Spoils of war ',
+    hour: '09:00 - 10:00',
+  },
+];
