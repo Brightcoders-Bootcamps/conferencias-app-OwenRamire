271b4b95d327045a0f4c63d6be3e894067b19b4d
OwenRamire <owenomar_ramirez@ucol.mx>
2020-07-16 18:28:52 -0500

    Show the old and new events in flatlist

--- OwenConferenciasApp/screens/CompletedEvents.js
+++ OwenConferenciasApp/screens/CompletedEvents.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {SafeAreaView, FlatList, StyleSheet, Text} from 'react-native';
+import CardEvent from '../components/cardEvent';
+import {OldEvents} from '../events/OldEvents';
 
-export default function Completed() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 25}}>Completed</Text>
-    </View>
-  );
+export default class Completed extends Component {
+  isEmpy = () => {
+    return <Text style={{ fontSize: 25, textAlign: 'center' }}>Completed ✅</Text>;
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.mainContainer}>
+        <FlatList
+          style={styles.yourEvents}
+          data={OldEvents}
+          renderItem={({item}) => <CardEvent data={item} />}
+          ListEmptyComponent={this.isEmpy}
+        />
+      </SafeAreaView>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  yourEvents: {
+    display: 'flex',
+  },
+  containerEmpy: {
+    // height: 80,
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'grey',
+  },
+});
--- OwenConferenciasApp/screens/UpcomingEvents.js
+++ OwenConferenciasApp/screens/UpcomingEvents.js
@@ -1,38 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {FlatList,View, StyleSheet, Text, SafeAreaView} from 'react-native';
 import CardEvent from '../components/cardEvent';
+import {NewEvents} from '../events/NewEvents';
 
 export default class Upcoming extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      events: [
-        {
-          key: '0',
-          date: '2020-07-06',
-          nameEvent: 'The Spoils of war ',
-          hour: '09:00 - 10:00',
-        },
-        {
-          key: '1',
-          date: '2020-07-07',
-          nameEvent: 'The broken man',
-          hour: '09:00 - 10:00',
-        },
-        {
-          key: '2',
-          date: '2020-07-08',
-          nameEvent: 'Back to winterfell',
-          hour: '09:00 - 10:00',
-        },
-      ],
-    };
-  }
-
   isEmpy = () => {
     return (
-      // <Text style={{ width: 280, fontSize: 25,  }}>No upcoming events 👀</Text>
-
       <View style={styles.containerEmpy}>
         <Text style={{ fontSize: 25, textAlign: 'center' }}>No upcoming events 👀</Text>
       </View>
@@ -41,10 +14,10 @@ export default class Upcoming extends Component {
 
   render() {
     return (
-      <SafeAreaView style={{flex: 1, alignItems: 'stretch', justifyContent: 'center', alignContent: 'center'}}>
+      <SafeAreaView style={styles.mainContainer}>
         <FlatList
           style={styles.yourEvents}
-          data={this.state.events}
+          data={NewEvents}
           renderItem={({item}) => <CardEvent data={item} />}
           ListEmptyComponent={this.isEmpy}
         />
@@ -53,28 +26,21 @@ export default class Upcoming extends Component {
   }
 }
 const styles = StyleSheet.create({
-  yourEvents: {
-    // borderWidth: 2,
-    // borderStyle: 'solid',
-    // borderColor: 'brown',
-    // display: 'flex',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#F7F7F8',
-  },
-  container: {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
+  mainContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  yourEvents: {
+    display: 'flex',
+  },
   containerEmpy: {
-    // height: 500,
-    // marginTop: 50,
-    flex: 1,
+    // height: 80,
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    flex: 3,
     justifyContent: 'center',
-    alignItems: 'stretch',
-    // backgroundColor: 'grey',
+    alignItems: 'center',
+    backgroundColor: 'grey',
   },
 });
